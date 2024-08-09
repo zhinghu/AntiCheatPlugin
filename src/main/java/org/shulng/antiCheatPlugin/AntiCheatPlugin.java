c7d7e756be8d56f134c38846321dfd035e0a11df
myChinesePYL <3223285598@qq.com>
2024-08-09 15:52:11 +0800

    write bug...

--- src/main/java/org/shulng/antiCheatPlugin/AntiCheatPlugin.java
+++ src/main/java/org/shulng/antiCheatPlugin/AntiCheatPlugin.java
@@ -9,11 +9,13 @@ public final class AntiCheatPlugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         HandySchedulerUtil.init(this);
+        getLogger().info("\u00a7k\u00a7a$\u00a7rAntiCheatPlugin running...");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("\u00a7k\u00a7c$\u00a7rAntiCheatPlugin is disable");
     }
 }
